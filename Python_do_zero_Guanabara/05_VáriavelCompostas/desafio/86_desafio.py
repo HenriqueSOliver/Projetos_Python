f96117d715828016f2d5e8134d6f45771bc93e1d
Henrique Silva Oliveira <HenriqueSOliver85@gmail.com>
2021-02-19 20:25:56 -0300

    Update 86_desafio.py

--- Python_do_zero_Guanabara/05_VáriavelCompostas/desafio/86_desafio.py
+++ Python_do_zero_Guanabara/05_VáriavelCompostas/desafio/86_desafio.py
@@ -1,9 +1,14 @@
 
-matriz = [[], [], []]
-val = 0
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-for c in range (0,3):
-    matriz.append(int(input(f'digite um valor: ')))
 
-print(matriz)
+for linha in range (0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: ')) 
+
+print("-"*30)
+for linha in range(0,3):
+    for coluna in range(0,3):
+        print(f'[{matriz [linha] [coluna]:^5}]', end='')
+    print()
 
