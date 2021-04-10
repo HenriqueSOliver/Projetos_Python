1e2f1e36f522c04709a5e75f8ab3fb22ba4c1b8a
Henrique Silva Oliveira <HenriqueSOliver85@gmail.com>
2021-04-09 21:48:28 -0300

    centralização do txt
    adicionei formatação no txt, com dois espaços

--- Python_do_zero_Guanabara/06_Metodos/desafio/97_desafio.py
+++ Python_do_zero_Guanabara/06_Metodos/desafio/97_desafio.py
@@ -1,11 +1,11 @@
 def tit(txt):
     tam = len(txt) + 4 # o tamanho será formatado de acordo com o tamanho do texto impresso
     print('~' * tam)
-    print(txt)
+    print(f'  {txt}') #para centralizar o texto inicial apenas apliquei o f e 2 espaços
     print('~' * tam)
 
 
 #Programa principal
-tit(f"{'Olá Mundo':^30}")
-tit(f"{'Sou Henrique':^30}")
+tit('Olá Mundo')
+tit('Sou Henrique')
 
